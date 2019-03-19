73092e859a96966fd9214d6d3b33ec6aa949fc74
Jonathan Hays <jon@silverpinesoftware.com>
2019-03-19 10:18:56 -0700

    Implemented uploading of photos

--- Example/Snippets-Example/ComposeViewController.swift
+++ Example/Snippets-Example/ComposeViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 import Snippets
 import UUSwift
 
-class ComposeViewController: UIViewController, UITextViewDelegate {
+class ComposeViewController: UIViewController, UITextViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
 	var originalPost : SnippetsPost?
+	var attachedPhoto : UIImage?
 	
+	@IBOutlet var addPhotoButton : UIButton!
 	@IBOutlet var busyView : UIView!
 	@IBOutlet var textView : UITextView!
 	
@@ -25,12 +27,21 @@ class ComposeViewController: UIViewController, UITextViewDelegate {
 		
 		if let post = self.originalPost {
 			self.textView.text = "@" + post.owner.userHandle + " "
+			
+			//
+			self.addPhotoButton.isHidden = true
 		}
 		
 		self.textView.becomeFirstResponder()
 		self.busyView.isHidden = true
     }
-    
+	
+	@IBAction func onAddPhoto() {
+		let imagePicker = UIImagePickerController()
+		imagePicker.delegate = self
+		self.present(imagePicker, animated: true, completion: nil)
+	}
+	
 	@objc func onCancel() {
 		self.navigationController?.dismiss(animated: true, completion: nil)
 	}
@@ -38,15 +49,49 @@ class ComposeViewController: UIViewController, UITextViewDelegate {
 	@objc func onPost() {
 		self.busyView.isHidden = false
 
-		if originalPost == nil {
+		if let photo = self.attachedPhoto {
+		
+			Snippets.shared.uploadImage(image: photo) { (error, url) in
+				
+				if let path = url {
+					let photos = [path]
+
+					Snippets.shared.postText(title: "", content: self.textView.text, photos: photos, altTags: []) { (error, path) in
+						DispatchQueue.main.async {
+							self.navigationController?.dismiss(animated: true, completion: nil)
+						}
+					}
+				}
+			}
+		}
+		else if originalPost == nil {
 			Snippets.shared.postText(title: "", content: self.textView.text, photos: [], altTags: []) { (error, path) in
-				self.navigationController?.dismiss(animated: true, completion: nil)
+				DispatchQueue.main.async {
+					self.navigationController?.dismiss(animated: true, completion: nil)
+				}
 			}
 		}
 		else {
 			Snippets.shared.reply(originalPost: self.originalPost!, content: self.textView.text) { (error) in
-				self.navigationController?.dismiss(animated: true, completion: nil)
+				DispatchQueue.main.async {
+					self.navigationController?.dismiss(animated: true, completion: nil)
+				}
 			}
+		}
+	}
+
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		var pickedImage = info[UIImagePickerController.InfoKey.editedImage]
+		if (pickedImage == nil) {
+			pickedImage = info[UIImagePickerController.InfoKey.originalImage]
+		}
+		
+		if let image = pickedImage as? UIImage {
+			self.attachedPhoto = image
+			self.addPhotoButton.setTitle(nil, for: .normal)
+			self.addPhotoButton.setBackgroundImage(image, for: .normal)
+			
+			picker.dismiss(animated: true, completion: nil)
 		}
 	}
 
